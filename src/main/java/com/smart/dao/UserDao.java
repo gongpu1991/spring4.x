97edee5081c42fe36d9671e7678c490b12df9dfe
gongpu1991 <303808367@qq.com>
2018-01-09 23:43:52 +0800

    创建了简单的表和基本的框架，熟悉了JdbcTemplate的使用

---
+++ src/main/java/com/smart/dao/UserDao.java
@@ -0,0 +1,23 @@
+package com.smart.dao;
+
+import com.smart.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDao {
+    //1.不使用mybatis
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate=jdbcTemplate;
+    }
+
+    public int addUser(User user){
+        String sql="insert into user(user_name,credits,password,last_visit,last_ip)values('gongpu',1,'123456',new DATE(),'127.0.0.1')";
+        jdbcTemplate.execute(sql);
+        return 0;
+    }
+}
---
+++ src/main/java/com/smart/main/Application.java
@@ -0,0 +1,11 @@
+package com.smart.main;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String args[]){
+        SpringBootApplication.run(Application.class,args);
+    }
+}
